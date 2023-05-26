e8cae6ce5db657cd0f9ad5048fb0f5c249ee7f42
ashkans <61254741+ashkans@users.noreply.github.com>
2023-05-26 17:15:11 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='world2d',
+    version='0.1',
+    author='Ashkan Shokri',
+    author_email='ashkn.shokri@gmail.com',
+    description='Description of your package',
+    packages=find_packages(),
+    install_requires=[
+        'dependency1',        
+    ],
+)
